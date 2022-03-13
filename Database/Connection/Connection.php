283ba9063409cac388c8f131fe45deafb8a1d094
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2022-03-13 21:28:18 +0300

    database connection construct function and query execute function was added

---
+++ Database/Connection/Connection.php
@@ -0,0 +1,34 @@
+<?php
+
+    namespace Database\Connection;
+
+    use \PDO;
+
+    class Connection
+    {
+        private $dbh;
+
+        function __construct()
+        {
+            $host = "";
+            $database = "";
+            $dsn = $host.$database;
+            $user = "root";
+            $password = "";
+
+            try {
+                $this->dbh = new PDO($dsn, $user, $password);
+                $this->dbh->exec("SET NAMES 'UTF8'; SET CHARSET 'UTF8'");
+                $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+            } catch (\PDOException $e) {
+                print $e->getMessage();
+            }
+        }
+
+        public function exec($query)
+        {
+            $this->dbh->exec($query);
+        }
+    }
+
+?>
